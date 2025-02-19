30eb4ee26457211b5618bdd04c69ba757899ce40
Nitin V H <nikki@Nitins-MacBook-Air-3.local>
2025-02-18 18:19:03 -0600

    Final ConFu

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from src.datascience import logger
+from src.datascience.pipeline.data_ingestion_pipeline import STAGE_NAME, DataIngestionTrainingPipeline
 
-logger.info("welcome to our project")
+try:
+    logger.info('{STAGE_NAME}<<<')
+    obj = DataIngestionTrainingPipeline()
+    obj.initiate_data_ingestion()
+    
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
